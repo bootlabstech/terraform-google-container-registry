626d8756be2382ada544ec98fd28e14e6a8e5c13
Karthick Dharman <karthick.dharman@bootlabstech.com>
2022-01-09 20:26:32 +0530

    <No ID> <terraform template standardisation>

--- provider.tf
+++ provider.tf
@@ -1,7 +1,9 @@
-  terraform {
+terraform {
+  required_version = "1.1.2"
+
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "4.1.0"
     }
   }
